8402bc7f929a2b1bcdc4624d53bf2e2c887d187e
Northman94 <Zhan2000@bigmir.net>
2018-09-06 12:15:11 +0300

    Grid Creation

---
+++ Test-Forest-U/Assets/ObjPlacer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjPlacer : MonoBehaviour 
+{
+
+    private Grid grid;
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>( );
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            RaycastHit rayHitInfo;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out rayHitInfo))
+            {
+                ObjPlacement(rayHitInfo.point);
+            }
+        }
+    }
+
+
+
+         void ObjPlacement (Vector3 nearPoint)
+        {
+            var finalPosition = grid.GetNearestPointOnGreed(nearPoint);
+
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+        }
+
+
+   
+
+
+}
